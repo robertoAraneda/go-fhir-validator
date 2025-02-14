43bdae553e4e822acfcd20a7bdb32f6d8d49b1d1
Roberto Araneda Espinoza <robaraneda@gmail.com>
2025-02-13 22:33:30 -0300

    fix: structure folder and load spec files

--- example/main.go
+++ example/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/robertoAraneda/go-fhir-validator/v1"
+	"github.com/robertoAraneda/go-fhir-validator/pkg/v1"
+	"log"
 )
 
 func main() {
+	// Initialize JSON data in memory
+	_, err := v1.LoadData()
+	if err != nil {
+		log.Fatalf("Error loading JSON data: %v", err)
+	}
+
 	data, err := v1.ReadJSONFile("resource.json")
 	if err != nil {
 		fmt.Println(err)
